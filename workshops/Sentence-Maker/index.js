bb3f2552ce487d3c409a39d5b96da2940b3a71da
lino ela <jamelarry22@gmail.com>
2026-01-12 22:39:47 +0100

    WorkShop | Sentence Make Hecho

---
+++ workshops/Sentence-Maker/index.js
@@ -0,0 +1,19 @@
+let adjective = "Objetivo";
+let noun = "Noun";
+let verb = "Verb";
+let place = "Place";
+let adjective2 = "Objetivo 2";
+let noun2 = "Noun";
+
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " +noun2+ ". The "+noun+" lived in a " +place+ " and had " +adjective2+" nostrils that blew fire when it was " +verb+".";
+console.log("First story: "+ firstStory);
+
+adjective = "Objetivo 2";
+noun = "Noun 2";
+verb = "Verb";
+place = "Place";
+adjective2 = "Nuevo Objetivo 2";
+noun2 = "Noun 2";
+
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " +noun2+ ". The "+noun+" lived in a " +place+ " and had " +adjective2+" nostrils that blew fire when it was " +verb+".";
+console.log("Second story: "+ secondStory);
